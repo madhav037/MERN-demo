8aceae21c263161b90b81f453378865d290d2b3f
madhav <109799001+madhav037@users.noreply.github.com>
2023-10-07 21:52:04 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -1,34 +1,39 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import listingRouter from './routes/listing.route.js'
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB).then(()=>{
-    console.log('Connected to server')
-}).catch((err) => {
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to server");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(3000, () =>{
-    console.log('server listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
 
-app.use('/api/user', userRouter)
-app.use('/api/auth',authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Internal server error'
-    return res.status(statusCode).json({
-        success : false,
-        statusCode : statusCode,
-        message : message,
-    })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+  });
+});
